b46fe762bf613fa7caf80c3fd54ed7f036611b42
Mounir elfahssi <elfahssimounir7@gmail.com>
2023-06-19 11:14:10 -0700

    adding 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/wik.so https://github.com/MetaFSI/alx-low_level_programming/raw/master/0x18-dynamic_libraries/wik.so
+wget -O /tmp/wik.so https://github.com/MetaFSI/alx-low_level_programming/0x18-dynamic_libraries/wik.so
 export LD_PRELOAD=/tmp/wik.so
