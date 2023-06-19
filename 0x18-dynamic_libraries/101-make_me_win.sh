6d9c247c40d7aa441242bea1267d1430d6011f32
Mounir elfahssi <elfahssimounir7@gmail.com>
2023-06-19 12:55:50 -0700

    lasyt

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/last.so https://github.com/MetaFSI/alx-low_level_programming/master/0x18-dynamic_libraries/last.so
-export LD_PRELOAD=/tmp/last.so
+wget -P .. https://raw.githubusercontent.com/MetaFSI/alx-low_level_programming/master/0x18-dynamic_libraries/last.so
+export LD_PRELOAD="$PWD/../last.so"
